29a8ae3c64a908c4f45faf3dd8d4edb58ad98744
SatoHaruta <poruting@gmail.com>
2023-11-04 03:56:10 +0900

    frameCountを実装

--- source/sketch.js
+++ source/sketch.js
@@ -10,11 +10,12 @@ export const sketch = (p) => {
     let ballsManager;
     let scene;
     let paneManager;
+    let frameCount = 0;
 
     p.setup = () => {
         scene = 0;
         canvasManager = new CanvasManager(p);
-        canvasManager.createCanvas(300,300);
+        canvasManager.createCanvas(300, 300);
         fileList = new FileList("fileList");
         paneManager = new PaneManager();
         fileList.event.add("svgFileAdded", () => {
@@ -24,28 +25,39 @@ export const sketch = (p) => {
             canvasManager.resizeCanvas(fileList.analyzer.svgViewSize.width, fileList.analyzer.svgViewSize.height);
             //sceneを更新する
             scene = 1;
-            
+
         });
 
-        window.addEventListener("keypress", () => {
-            ballsManager.reset();
+        window.addEventListener("keypress", (event) => {
+            if (scene == 1) {
+                if (event.key === " ") {
+                    ballsManager.reset();
+                    frameCount = 0;
+                }
+            }
         });
         paneManager.event.add("PARAMSChanged", (PARAMS) => {
             ballsManager.setPARAMS(PARAMS);
             ballsManager.reset();
+            frameCount = 0;
         });
     };
 
     p.draw = () => {
         p.background(220);
-        if(scene == 1){
+        if (scene == 1) {
             scene1();
         }
     };
 
-    function scene1(){
+    function scene1() {
         ballsManager.update();
         ballsManager.display();
+        p.noStroke();
+        p.fill(0);
+        p.textSize(30);
+        p.text("現在のフレーム: " + frameCount, 0, 30);
+        frameCount++;
     }
 };
 
