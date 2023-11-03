ab24efbb2e44ece7ad1b4553a6509114fee7425f
SatoHaruta <poruting@gmail.com>
2023-11-04 01:37:49 +0900

    PaneManagerにeventを実装

--- source/PaneManager.js
+++ source/PaneManager.js
@@ -1,4 +1,5 @@
 import { Pane } from "tweakpane";
+import { EventListener } from "./utility/eventListener/eventListener";
 
 
 export class PaneManager {
@@ -6,6 +7,10 @@ export class PaneManager {
     PARAMS;
 
     constructor() {
+        this.event = new EventListener();
+        this.event.add("PARAMSChanged");
+        this.event.add("encodeButtonPressed");
+
         this.pane = new Pane();
         this.PARAMS = {
             Gravity: 350,
@@ -13,25 +18,49 @@ export class PaneManager {
             Speed: 0.017,
             LimitSpeed: 0.7,
         };
-        this.addBindings();
+        this.PARAMSFolder = this.pane.addFolder({
+            title: 'パラメーター',
+        });
+        this.buttonFolder = this.pane.addFolder({
+            title: 'ボタン',
+        });
+        this.addPARAMSBindings();
+        this.addButtonBindings();
     }
-    addBindings(){
-        this.pane.addBinding(this.PARAMS, 'Gravity', {
+    addPARAMSBindings() {
+        this.PARAMSFolder.addBinding(this.PARAMS, 'Gravity', {
             min: 100,
             max: 500,
         });
-        this.pane.addBinding(this.PARAMS, 'Speed', {
+        this.PARAMSFolder.addBinding(this.PARAMS, 'Speed', {
             min: 0,
             max: 0.1,
         });
-        this.pane.addBinding(this.PARAMS, 'LimitSpeed', {
+        this.PARAMSFolder.addBinding(this.PARAMS, 'LimitSpeed', {
             min: 0,
             max: 5,
         });
-        this.pane.addBinding(this.PARAMS, 'InitSpeedRatio', {
+        this.PARAMSFolder.addBinding(this.PARAMS, 'InitSpeedRatio', {
             step: 0.1,
             min: 0,
             max: 1,
+        });
+        this.PARAMSFolder.on('change', (ev) => {
+            if (ev.last) {
+                console.log("Paneで値を操作した");
+                this.event.dispatch("PARAMSChanged", this.PARAMS);
+            }
+        });
+    }
+
+    addButtonBindings() {
+        this.encodeButton = this.buttonFolder.addButton({
+            title: 'スタート',
+            label: 'エンコード',
+        });
+        this.encodeButton.on('click', () => {
+            console.log("エンコードボタンが押された");
+            this.event.dispatch("encodeButtonPressed");
         });
     }
 }
