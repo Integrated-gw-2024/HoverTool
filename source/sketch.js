37fb28a78947253caf4e7b1132164e6c2c32b820
SatoHaruta <poruting@gmail.com>
2023-11-04 04:17:31 +0900

    書き出し機能をとりあえず実装

--- source/sketch.js
+++ source/sketch.js
@@ -2,6 +2,7 @@ import { CanvasManager } from "./CanvasManager";
 import { FileList } from "./fileList/FileList";
 import { BallsManager } from "./BallsManager";
 import { PaneManager } from "./PaneManager";
+import { CanvasCapture } from 'canvas-capture';
 
 
 export const sketch = (p) => {
@@ -14,8 +15,10 @@ export const sketch = (p) => {
 
     p.setup = () => {
         scene = 0;
+
         canvasManager = new CanvasManager(p);
         canvasManager.createCanvas(300, 300);
+
         fileList = new FileList("fileList");
         paneManager = new PaneManager();
         fileList.event.add("svgFileAdded", () => {
@@ -25,8 +28,13 @@ export const sketch = (p) => {
             canvasManager.resizeCanvas(fileList.analyzer.svgViewSize.width, fileList.analyzer.svgViewSize.height);
             //sceneを更新する
             scene = 1;
-
         });
+
+        // Initialize and pass in canvas.
+        CanvasCapture.init(
+            document.getElementById('defaultCanvas0'),
+            { showRecDot: true }, // Options are optional, more info below.
+        );
 
         window.addEventListener("keypress", (event) => {
             if (scene == 1) {
@@ -41,16 +49,37 @@ export const sketch = (p) => {
             ballsManager.reset();
             frameCount = 0;
         });
+        paneManager.event.add("encodeButtonPressed", () => {
+            frameCount = 0;
+            CanvasCapture.beginPNGFramesRecord({
+                onExportProgress: (progress) => {
+                    console.log(progress);
+                },
+                fps: 60,
+                name: 'pngFrames',
+            })
+            scene = 2;
+        });
     };
 
     p.draw = () => {
-        p.background(220);
+        if (scene == 0) {
+            scene0();
+        }
         if (scene == 1) {
             scene1();
         }
+        if (scene == 2) {
+            scene2();
+        }
     };
 
+    function scene0() {
+        p.background(220);
+    }
+
     function scene1() {
+        p.background(220);
         ballsManager.update();
         ballsManager.display();
         p.noStroke();
@@ -59,5 +88,21 @@ export const sketch = (p) => {
         p.text("現在のフレーム: " + frameCount, 0, 30);
         frameCount++;
     }
+
+    function scene2() {
+        p.clear();
+        ballsManager.update();
+        ballsManager.display();
+        if (frameCount == 50) {
+            CanvasCapture.stopRecord();
+            scene = 1;
+        }
+        frameCount++;
+        if (CanvasCapture.isRecording()) {
+            CanvasCapture.recordFrame();
+        }
+    }
+
+
 };
 
