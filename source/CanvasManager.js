0c48e78ca3a463d9d569dcf9c90f562e83f414d6
SatoHaruta <poruting@gmail.com>
2023-11-05 11:28:31 +0900

    canvasのサイズを可変にした

--- source/CanvasManager.js
+++ source/CanvasManager.js
@@ -1,14 +1,47 @@
-export class CanvasManager{
+export class CanvasManager {
     p;
+    canvas;
+    aspectRatio;
+    widthLimit;
+    heightLimit;
+    panelsWidth;
 
-    constructor(P5){
+    constructor(P5) {
         this.p = P5;
+        this.canvas = document.getElementById('defaultCanvas0');
+        this.panelsWidth = 320;
+        this.widthLimit = window.innerWidth - this.panelsWidth;
+        this.heightLimit = window.innerHeight- 20;
     }
 
-    resizeCanvas(Width, Height){
+    resizeCanvas(Width, Height) {
+        console.log(this.aspectDetector(Width, Height));
         this.p.resizeCanvas(Width, Height);
+        if (this.aspectDetector(Width, Height) == "alignWidth") {
+            let widthRatio = this.widthLimit / Width;
+            this.canvas.style.width = `${Width * widthRatio}px`;
+            this.canvas.style.height = `${Height * widthRatio}px`;
+        }
+        else if (this.aspectDetector(Width, Height) == "alignHeight") {
+            let heightRatio = this.heightLimit / Height;
+            this.canvas.style.width = `${Width * heightRatio}px`;
+            this.canvas.style.height = `${Height * heightRatio}px`;
+        }
     }
-    createCanvas(Width, Height){
+    createCanvas(Width, Height) {
+        this.aspectRatio = Width / Height;
+        console.log(this.aspectDetector(Width, Height))
         this.p.createCanvas(Width, Height);
+        this.canvas.style.width = '100px';
+        this.canvas.style.height = '100px';
+    }
+
+    aspectDetector(Width, Height) {
+        let widthRatio = this.widthLimit / Width;
+        if (Height * widthRatio > this.heightLimit) {
+            return "alignHeight";
+        } else {
+            return "alignWidth";
+        }
     }
 }
